5c18d18f6bf76144a6fb5245c62cbd734fda3ad5
Sanket Mandaliya <sanketmandaliya52@gmail.com>
2024-10-31 14:48:19 +0530

    Service logic for negative number not allowed

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,11 +1,13 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    if numbers.start_with?("//")
-      delimiter, numbers = numbers[2..].split("\n", 2)
-      numbers.split(delimiter).map(&:to_i).sum
-    else
-      numbers.split(/,|\n/).map(&:to_i).sum
-    end
+    delimiter = numbers.start_with?("//") ? numbers[2..].split("\n", 2).first : /,|\n/
+    numbers = numbers.split("\n", 2).last if numbers.start_with?("//")
+    nums = numbers.split(delimiter).map(&:to_i)
+
+    negatives = nums.select(&:negative?)
+    raise "negative numbers not allowed: #{negatives.join(", ")}" if negatives.any?
+
+    nums.sum
   end
 end
