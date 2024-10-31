30e0aebc23c42785cef0bb6ac322992e4046ff5f
Sanket Mandaliya <sanketmandaliya52@gmail.com>
2024-10-31 14:38:08 +0530

    Service logic for custom delimiter

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,6 +1,11 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..].split("\n", 2)
+      numbers.split(delimiter).map(&:to_i).sum
+    else
+      numbers.split(/,|\n/).map(&:to_i).sum
+    end
   end
 end
