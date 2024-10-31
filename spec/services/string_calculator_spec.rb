a00904f240c759464f17ee242728bdcb252e7a5c
Sanket Mandaliya <sanketmandaliya52@gmail.com>
2024-10-31 14:13:55 +0530

    Spec for only one number in string

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "returns the number itself if only one number is provided" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
   end
 end
